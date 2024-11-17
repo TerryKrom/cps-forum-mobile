28d770f466887ac327a3bb8eaec290997ae4a063
TerryKrom <terrykrom1@gmail.com>
2024-11-17 20:12:54 -0300

    FIX: Create topic keyboard fix

--- src/app/(defaultLayout)/auth/page.jsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Auth() {
-    redirect('/sign-in');
-    //Caso a página for acessada sem ser através do botão de login, o usuário retorna para a home
-}
--- src/app/(defaultLayout)/criar/page.jsx
+++ src/app/(defaultLayout)/criar/page.jsx
@@ -6,6 +6,10 @@ import {
   TouchableOpacity,
   ScrollView,
   StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -37,26 +41,23 @@ const Criar = () => {
   const [selectedTags, setSelectedTags] = useState([]);
   const [inputTag, setInputTag] = useState('');
 
-  // Função para enviar o formulário
   const onSubmit = async (values) => {
     values.tags = selectedTags;
     const { title, privacy, section, body } = values;
 
     const requestBody = {
-      userId: 2, // Substitua por session data, se aplicável
-      title: title,
-      privacy: privacy,
-      section: section,
-      body: body,
+      userId: 2,
+      title,
+      privacy,
+      section,
+      body,
       tag: 'Tag Example',
     };
 
     try {
       const req = await fetch('https://cpsforum.com.br/api/criar', {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestBody),
       });
 
@@ -85,136 +86,143 @@ const Criar = () => {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.header}>Criar Tópico</Text>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text style={styles.header}>Criar Tópico</Text>
 
-      {/* Título */}
-      <View style={styles.formGroup}>
-        <Text style={styles.label}>Título</Text>
-        <Controller
-          control={control}
-          name="title"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              style={styles.input}
-              placeholder="Digite o título do tópico"
-              value={value}
-              onChangeText={onChange}
+          {/* Título */}
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Título</Text>
+            <Controller
+              control={control}
+              name="title"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  style={styles.input}
+                  placeholder="Digite o título do tópico"
+                  value={value}
+                  onChangeText={onChange}
+                />
+              )}
             />
-          )}
-        />
-        {errors.title && <Text style={styles.error}>{errors.title.message}</Text>}
-        {step === 1 && (
-          <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
-            <Text style={styles.buttonText}>Próximo</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-
-      {/* Privacidade */}
-      {step >= 2 && (
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Privacidade</Text>
-          <Controller
-            control={control}
-            name="privacy"
-            render={({ field: { onChange, value } }) => (
-              <Picker selectedValue={value} onValueChange={onChange} style={styles.picker}>
-                <Picker.Item label="Público" value="public" />
-                <Picker.Item label="Minha instituição" value="institution" />
-                <Picker.Item label="Privado" value="private" />
-              </Picker>
-            )}
-          />
-          {step === 2 && (
-            <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
-              <Text style={styles.buttonText}>Próximo</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-      )}
-
-      {/* Seção */}
-      {step >= 3 && (
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Seção</Text>
-          <Controller
-            control={control}
-            name="section"
-            render={({ field: { onChange, value } }) => (
-              <Picker selectedValue={value} onValueChange={onChange} style={styles.picker}>
-                {sectionlinks.slice(1).map((section, index) => (
-                  <Picker.Item key={index} label={section.title} value={section.slug} />
-                ))}
-              </Picker>
-            )}
-          />
-          {step === 3 && (
-            <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
-              <Text style={styles.buttonText}>Próximo</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-      )}
-
-      {/* Tags */}
-      {step >= 4 && (
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Tags (opcional)</Text>
-          <View style={styles.tagsContainer}>
-            {selectedTags.map((tag, index) => (
-              <TouchableOpacity key={index} onPress={() => removeTag(tag)} style={styles.tag}>
-                <Text style={styles.tagText}>{tag}</Text>
+            {errors.title && <Text style={styles.error}>{errors.title.message}</Text>}
+            {step === 1 && (
+              <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
+                <Text style={styles.buttonText}>Próximo</Text>
               </TouchableOpacity>
-            ))}
-          </View>
-          <TextInput
-            style={styles.input}
-            placeholder="Adicionar tag"
-            value={inputTag}
-            onChangeText={setInputTag}
-            onSubmitEditing={() => handleTags(inputTag)}
-          />
-          <TouchableOpacity style={styles.button} onPress={() => handleTags(inputTag)}>
-            <Text style={styles.buttonText}>Adicionar Tag</Text>
-          </TouchableOpacity>
-          {step === 4 && (
-            <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
-              <Text style={styles.buttonText}>Próximo</Text>
-            </TouchableOpacity>
-          )}
-        </View>
-      )}
-
-      {/* Corpo */}
-      {step >= 5 && (
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Corpo</Text>
-          <Controller
-            control={control}
-            name="body"
-            render={({ field: { onChange, value } }) => (
-              <TextInput
-                style={[styles.input, styles.textArea]}
-                placeholder="Escreva o corpo do tópico"
-                value={value}
-                onChangeText={onChange}
-                multiline
-              />
             )}
-          />
-          {errors.body && <Text style={styles.error}>{errors.body.message}</Text>}
-        </View>
-      )}
+          </View>
 
-      <TouchableOpacity
-        style={[styles.button, step < 5 && { backgroundColor: '#ccc' }]}
-        onPress={handleSubmit(onSubmit)}
-        disabled={step < 5}
-      >
-        <Text style={styles.buttonText}>Criar Tópico</Text>
-      </TouchableOpacity>
-    </ScrollView>
+          {/* Privacidade */}
+          {step >= 2 && (
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Privacidade</Text>
+              <Controller
+                control={control}
+                name="privacy"
+                render={({ field: { onChange, value } }) => (
+                  <Picker selectedValue={value} onValueChange={onChange} style={styles.picker}>
+                    <Picker.Item label="Público" value="public" />
+                    <Picker.Item label="Minha instituição" value="institution" />
+                    <Picker.Item label="Privado" value="private" />
+                  </Picker>
+                )}
+              />
+              {step === 2 && (
+                <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
+                  <Text style={styles.buttonText}>Próximo</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          )}
+
+          {/* Seção */}
+          {step >= 3 && (
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Seção</Text>
+              <Controller
+                control={control}
+                name="section"
+                render={({ field: { onChange, value } }) => (
+                  <Picker selectedValue={value} onValueChange={onChange} style={styles.picker}>
+                    {sectionlinks.slice(1).map((section, index) => (
+                      <Picker.Item key={index} label={section.title} value={section.slug} />
+                    ))}
+                  </Picker>
+                )}
+              />
+              {step === 3 && (
+                <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
+                  <Text style={styles.buttonText}>Próximo</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          )}
+
+          {/* Tags */}
+          {step >= 4 && (
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Tags (opcional)</Text>
+              <View style={styles.tagsContainer}>
+                {selectedTags.map((tag, index) => (
+                  <TouchableOpacity key={index} onPress={() => removeTag(tag)} style={styles.tag}>
+                    <Text style={styles.tagText}>{tag}</Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+              <TextInput
+                style={styles.input}
+                placeholder="Adicionar tag"
+                value={inputTag}
+                onChangeText={setInputTag}
+                onSubmitEditing={() => handleTags(inputTag)}
+              />
+              <TouchableOpacity style={styles.button} onPress={() => handleTags(inputTag)}>
+                <Text style={styles.buttonText}>Adicionar Tag</Text>
+              </TouchableOpacity>
+              {step === 4 && (
+                <TouchableOpacity style={styles.button} onPress={() => setStep(step + 1)}>
+                  <Text style={styles.buttonText}>Próximo</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          )}
+
+          {/* Corpo */}
+          {step >= 5 && (
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Corpo</Text>
+              <Controller
+                control={control}
+                name="body"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput
+                    style={[styles.input, styles.textArea]}
+                    placeholder="Escreva o corpo do tópico"
+                    value={value}
+                    onChangeText={onChange}
+                    multiline
+                  />
+                )}
+              />
+              {errors.body && <Text style={styles.error}>{errors.body.message}</Text>}
+            </View>
+          )}
+
+          <TouchableOpacity
+            style={[styles.button, step < 5 && { backgroundColor: '#ccc' }]}
+            onPress={handleSubmit(onSubmit)}
+            disabled={step < 5}
+          >
+            <Text style={styles.buttonText}>Criar Tópico</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -222,7 +230,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     backgroundColor: '#ffffff',
-    height: '100%',
+    flexGrow: 1,
   },
   header: {
     fontSize: 24,
@@ -278,12 +286,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#4594ff',
     borderRadius: 5,
     alignItems: 'center',
-    marginVertical: 5,
   },
   buttonText: {
     color: '#ffffff',
     fontSize: 16,
-    fontWeight: 'bold',
   },
 });
 
